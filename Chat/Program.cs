c1824e625bd1c2ee17244acdf6ed434bcb02e226
Андрей Рудзитис <andreyrud@inbox.lv>
2022-03-18 20:49:12 +0300

    first commit

---
+++ Chat/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    internal class Program
+    {
+        static UdpClient client;
+        static IPEndPoint serverEp;
+        const int serverPort = 2544;
+
+        static async Task<string> SendAndReceiveCommand(string command)
+        {
+            byte[] commandDatagram = Encoding.UTF8.GetBytes(command);
+            await client.SendAsync(commandDatagram, commandDatagram.Length, serverEp);
+            UdpReceiveResult result = await client.ReceiveAsync();
+            string reply = Encoding.UTF8.GetString(result.Buffer);
+            return reply;
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            client = new UdpClient();
+            serverEp = new IPEndPoint(IPAddress.Loopback, serverPort);
+            string command = null;
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            string reply = await SendAndReceiveCommand($"Register;{username}");
+            if (reply == "Complete")
+                Console.WriteLine("User created");
+            else
+                Console.WriteLine("Authorized");
+            while (command != "exit")
+            {
+                Console.Write("Enter command: ");
+                command = Console.ReadLine();
+
+                if (command != "exit")
+                {
+                    switch (command)
+                    {
+                        case "Send":
+                            {
+                                Console.WriteLine("Enter message: ");
+                                string msg = Console.ReadLine();
+                                Console.WriteLine("Enter user: ");
+                                string receiver = Console.ReadLine();
+                                reply = await SendAndReceiveCommand($"Send;{username};{receiver};{msg}");
+                                break;
+                            }
+
+                        case "Receive":
+                            {
+                                Console.WriteLine("Enter for how many days: ");
+                                int days = int.Parse(Console.ReadLine());
+                                reply = await SendAndReceiveCommand($"Receive;{username};{DateTime.Now.AddDays(-days)}");
+                                break;
+                            }
+                    }
+                }
+                Console.WriteLine(reply);
+            }
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static UdpClient server;
+        const int serverPort = 2544;
+        static bool isRunning;
+
+        static async Task Listen()
+        {
+            using (ChatContext chatContext = new ChatContext())
+            {
+                chatContext.Database.EnsureCreated();
+                while (isRunning)
+                {
+                    UdpReceiveResult commandDatagram = await server.ReceiveAsync();
+                    string command = Encoding.UTF8.GetString(commandDatagram.Buffer);
+                    string[] parts = command.Split(';');
+                    bool result = false;
+                    string reply = "";
+                    StringBuilder sb = new StringBuilder();
+                    List<Message> messages = new List<Message>();
+                    switch (parts[0])
+                    {
+                        case "Register":
+                            result = await chatContext.RegisterUser(parts[1]);
+                            if (!result)
+                                reply = "Name already exists";
+                            else
+                                reply = "Complete";
+                            break;
+                        case "Send":
+                            result = await chatContext.SendMessage(parts[1], parts[2], parts[3]);
+                            if (!result)
+                                reply = "Could not send message";
+                            else
+                                reply = "Message sent";
+                            break;
+                        case "Receive":
+                            messages = await chatContext.GetAllMyMessages
+                                (parts[1], DateTime.Parse(parts[2]));
+                            foreach (Message message in messages)
+                            {
+                                sb.Append($"{message.Sender.Name}:{message.Text}\n");
+                            }
+                            reply = sb.ToString();
+                            break;
+                            
+                    }
+                    byte[] replyDatagram = Encoding.UTF8.GetBytes(reply);
+                    await server.SendAsync(replyDatagram, replyDatagram.Length, commandDatagram.RemoteEndPoint);
+
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            server = new UdpClient(serverPort);
+            isRunning = true;
+            Task listenTask = Listen();
+            string command = null;
+            while (command != "exit")
+                command = Console.ReadLine();
+            isRunning = false;
+            await listenTask;
+        }
+
+    }
+}
